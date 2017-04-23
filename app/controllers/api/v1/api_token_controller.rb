79298b80499182be685a7f33cc5dd797bc12c888
Adam Cardenas <cevaris@gmail.com>
2017-04-23 08:48:38 -0600

    api token email validation

--- app/controllers/api/v1/api_token_controller.rb
+++ app/controllers/api/v1/api_token_controller.rb
@@ -8,14 +8,12 @@ class Api::V1::ApiTokenController < ApplicationController
 
   def create
     @api_token = ApiToken.new(api_token_params)
-    puts @api_token.inspect
     if @api_token.save
       render json: @api_token.to_json
     else
       render json: {errors: @api_token.errors.full_messages}
     end
   end
-
 
   def api_token_params
     params.require(:api_token).permit(:email)
--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -3,6 +3,7 @@ require 'securerandom'
 class ApiToken < ApplicationRecord
   before_save :default_values
 
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates_uniqueness_of :email
   validates_uniqueness_of :token
 
