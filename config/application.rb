e1c8965ae4753c322b59898fc7f9c00b223bf55b
Adam Cardenas <cevaris@gmail.com>
2017-04-22 18:49:07 -0600

    added secrets

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Authsuite
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.api_only = true
+    config.debug_exception_response_format = :api
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
