5b0ef2b77a54254401c463b2fde9ff9e59b3ddf9
Adam Cardenas <cevaris@gmail.com>
2017-04-25 18:25:30 -0600

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
       namespace :v1 do
         get 'welcome', to: 'welcome#show'
 
-        get 'auth/token', to: 'api_token#show'
-        post 'auth/token', to: 'api_token#create'
+        get 'auth/tokens', to: 'api_token#show'
+        post 'auth/tokens', to: 'api_token#create'
 
-        get 'auth/session/status', to: 'auth_session#show_with_receipt'
-        post 'auth/session/token/accept', to: 'auth_session#token_accept'
-        post 'auth/session/token/reject', to: 'auth_session#token_reject'
-        post 'auth/session', to: 'auth_session#create'
+        get 'auth/sessions/receipts/:receipt/status', to: 'auth_session#show_with_receipt'
+        post 'auth/sessions/tokens/:token/accept', to: 'auth_session#token_accept'
+        post 'auth/sessions/tokens/:token/reject', to: 'auth_session#token_reject'
+        post 'auth/sessions', to: 'auth_session#create'
       end
     end
   end
