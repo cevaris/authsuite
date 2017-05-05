adc6153f61e065be2bb579bdd75a1c9275647bcb
Adam Cardenas <cevaris@gmail.com>
2017-05-05 08:18:32 -0600

    working on demo form

--- web/src/components/Demo/EmailDemo.jsx
+++ web/src/components/Demo/EmailDemo.jsx
@@ -1,36 +1,29 @@
-import { Button, Col, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Row } from 'react-bootstrap';
-import React, { Component } from 'react';
+import {Button, Col, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Row} from "react-bootstrap";
+import React, {Component} from "react";
 
 export default class EmailDemo extends Component {
   render() {
     return (
       <div className='demo-auth-session__form'>
-        <Row>
-          <Col sm={12} xs={12}>
 
-            <Form horizontal>
+        <Form>
 
-              <FormGroup>
-                <Col componentClass={ControlLabel} sm={3}>
-                  Email
-                </Col>
-                <Col sm={9}>
-                  <FormControl name='auth_session_demo[identity]' placeholder='email@example.com' type='email' />
-                  <HelpBlock>Demo using your Email</HelpBlock>
-                </Col>
-              </FormGroup>
+          <FormGroup validationState="error">
+            <ControlLabel>
+              Email
+            </ControlLabel>
+            <FormControl name='auth_session_demo[identity]' placeholder='email@example.com' type='email'/>
+            <HelpBlock>Demo using your Email</HelpBlock>
+            <FormControl.Feedback />
+          </FormGroup>
 
-              <FormGroup>
-                <Col sm={9} smOffset={3}>
-                  <Button bsStyle='primary' type='submit'>
-                    Send Email
-                  </Button>
-                </Col>
-              </FormGroup>
-            </Form>
+          <FormGroup>
+            <Button bsStyle='primary' type='submit' disabled>
+              Send Email
+            </Button>
+          </FormGroup>
+        </Form>
 
-          </Col>
-        </Row>
       </div>
     );
   }
--- web/src/components/Demo/SmsDemo.jsx
+++ web/src/components/Demo/SmsDemo.jsx
@@ -1,37 +1,28 @@
-import { Button, Col, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Row } from 'react-bootstrap';
-import React, { Component } from 'react';
+import {Button, Col, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Row} from "react-bootstrap";
+import React, {Component} from "react";
 
 export default class SmsDemo extends Component {
   render() {
     return (
       <div className='demo-auth-session__form'>
-        <Row>
 
-          <Col sm={12} xs={12}>
+        <Form>
+          <FormGroup>
+            <ControlLabel>
+              SMS/Text
+            </ControlLabel>
+            <FormControl name='identity' placeholder='555-123-4567' type='tel'/>
+            <HelpBlock>Demo using your SMS/Text Message</HelpBlock>
+            <FormControl.Feedback />
+          </FormGroup>
 
-            <Form horizontal>
+          <FormGroup>
+            <Button bsStyle='primary' type='submit' disabled>
+              Send Text
+            </Button>
+          </FormGroup>
+        </Form>
 
-              <FormGroup>
-                <Col componentClass={ControlLabel} sm={3}>
-                  SMS/Text
-                </Col>
-                <Col sm={9}>
-                  <FormControl name='auth_session_demo[identity]' placeholder='555-123-4567' type='tel'/>
-                  <HelpBlock>Demo using your SMS/Text Message</HelpBlock>
-                </Col>
-              </FormGroup>
-
-              <FormGroup>
-                <Col sm={9} smOffset={3}>
-                  <Button bsStyle='primary' type='submit'>
-                    Send Text
-                  </Button>
-                </Col>
-              </FormGroup>
-            </Form>
-
-          </Col>
-        </Row>
       </div>
     );
   }
