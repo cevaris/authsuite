17c88c6e16e88dc9a2e115634c00c340e5137378
Adam Cardenas <cevaris@gmail.com>
2017-05-07 07:18:30 -0600

    renaming web ApiToken to ApiKey

---
+++ web/src/components/ApiKey/ApiKey.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ApiKey extends Component {
+  render() {
+    return (
+      <div className='api-key'>
+        <p>ApiKey</p>
+      </div>
+    );
+  }
+}
